e18dd1ccf61add78bc3281e360920dbc245df393
kerr820725 <kerr820725@gmail.com>
2020-02-26 10:36:49 +0800

    image_test first commit

---
+++ image_test.py
@@ -0,0 +1,21 @@
+#!/usr/bin/env python
+# coding: utf-8
+
+
+import os 
+from PIL import Image
+
+def image_(image_name='',width='',hight=''):
+    global msg
+    try:
+        img_path = (os.path.join(os.path.expanduser("~"), 'Desktop'))
+        print(img_path)
+        a = os.path.join(img_path,image_name)
+        img = Image.open(a)
+        resize = img.resize((int(width),int(hight)),Image.ANTIALIAS)
+        resize.save(a)
+        #print(resize.size)
+        msg = '%s 調整大小成功'%image_name
+    except FileNotFoundError as e:
+        msg=('%s檔案不存在'%image_name)
+    #return img_path
