542d43f101edc32bf62f60cf569f8f4bd7df846e
kerr820725 <kerr820725@gmail.com>
2020-07-02 16:13:39 +0800

    chromedriver 動態路徑補上

--- Utils/Config.py
+++ Utils/Config.py
@@ -4,10 +4,9 @@ import os
 import AutoTest
 
 # ChromeDriver 取用路徑 (若環境參數無法獲取時取用)
-chromeDriver_Path = r'C:\Users\Wen\PycharmProjects\kerr_flask\chromedriver_83.exe'
-# report.html 絕對路徑
 path1=os.path.abspath('.')
-
+chromeDriver_Path = path1 + "\\chromedriver.exe"
+# report.html 絕對路徑
 reportHtml_Path = path1 + "\\templates\\report.html"  
 
 # ChromeDriver 設定參數
