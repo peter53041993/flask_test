40ac024f3f641c4968336dcda7c93461f5c67e53
kerr820725 <kerr820725@gmail.com>
2020-09-09 18:20:05 +0800

    增加共用 輸入框空白移除/dataframe error處理

--- static/js/test.js
+++ static/js/test.js
@@ -50,16 +50,30 @@ document.writeln("        </button>");
 document.writeln("    </div>");
  
 function pretty(js_path,split_){ //dataframe調整 文字版面 , js_path為element js定位, split_: 切各字元
-    game_explan = document.querySelector(js_path)//
-    explan_1 = game_explan.textContent.split(split_)[0]//用 #來分割
-    explan_2 = game_explan.textContent.split(split_)[1]// #後
-    game_explan.innerHTML = explan_1 + "<br>" +"<span>"+split_+explan_2+"</span>" //換行 ,增加span
+    try{
+        game_explan = document.querySelector(js_path)//
+        explan_1 = game_explan.textContent.split(split_)[0]//用 #來分割
+        explan_2 = game_explan.textContent.split(split_)[1]// #後
+        game_explan.innerHTML = explan_1 + "<br>" +"<span>"+split_+explan_2+"</span>" //換行 ,增加span
+    }
+    catch{
+        return false;
+    }
 }
-function remote_IP(){
+function remote_IP(){ // 在頁面,呼叫是哪個ip
     var ip = returnCitySN["cip"]+','+returnCitySN["cname"]
     console.log(ip)
     $.post(
         url = '/remote_IP',
         data = ip
     )
+}
+function form_trim(form_element){ // 輸入框,去除 空白
+    var form = $(form_element);
+    var formArr = form. serializeArray();
+    jQuery.each(formArr , function(i, field) {
+    formArr[i].value = $.trim(field.value);
+    });
+    var serializedForm = $.param(formArr);
+    return serializedForm
 }
